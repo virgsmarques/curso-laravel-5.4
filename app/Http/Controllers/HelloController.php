cf6d5efc181f794d5ea8400a2c89752b84afd7d8
virgsmarques <vmarques13@hotmail.com>
2018-07-23 21:27:34 -0500

    Alterações referente a aula blade e primeira aplicação

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller {
+    public function index($name = 'world'){
+    	return view('hello', ['n'=>$name]); 
+    }
+
+    public function render(Request $request){
+    	return 'Hello ' . $request->input('name');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,24 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+
+* Informaçõoes do professor
+*
+* get ('rota', function() {})
+* post ('rota', function() {})
+* delete ('rota', function() {})
+* put ('rota', function() {}) 
+* path ('rota', function() {})
+* options ('rota', function() {})
+* match (['get','post'], rota', function() {})
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name?}', 'HelloController@index');
+Route::post('/hello/{name?}', 'HelloController@render');
+
+// GET é responsavel por acesso a rotas
